c9a152190372f459f8d0c71a9a1242a6115db6f0
Robert Mayer <rwlmayer@gmail.com>
2018-01-14 14:06:23 +0100

    Improved dependency injection and created repository

---
+++ source/businesstools/businesstools/Data/Repositories/BusinessModelCanvasRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace businesstools.Data.Repositories
+{
+    public class BusinessModelCanvasRepository
+    {
+        public BusinessModelCanvasRepository()
+        {
+        }
+    }
+}
